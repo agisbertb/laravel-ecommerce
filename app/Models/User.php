c05e7984572784f7b7b56f5e6bf4c36974c5ce14
agisbertb <agisbert@iesebre.com>
2024-03-07 16:46:07 +0100

    Fix Model User for new config

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,4 +62,24 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+
+    public function addresses()
+    {
+        return $this->hasMany(Address::class);
+    }
+
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+
+
+
 }
