d45bf5d750b102423610f76e32f42dcdc4bb8637
Andreu Gisbert Bel <91249151+agisbertb@users.noreply.github.com>
2024-05-22 07:28:03 +0200

    Style of cookies banner changed

--- resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -1,9 +1,9 @@
 <div class="js-cookie-consent cookie-consent fixed bottom-0 inset-x-0 pb-2">
     <div class="max-w-7xl mx-auto px-6">
-        <div class="p-2 rounded-lg bg-blue-100">
+        <div class="p-2 rounded-lg bg-gray-900">
             <div class="flex items-center justify-between flex-wrap">
                 <div class="w-0 flex-1 items-center hidden md:inline">
-                    <p class="ml-3 text-gray-900 cookie-consent__message">
+                    <p class="ml-3 text-white cookie-consent__message font-bold">
                         {!! trans('cookie-consent::texts.message') !!}
                     </p>
                 </div>
@@ -11,7 +11,7 @@
                     <button class="js-cookie-consent-agree cookie-consent__agree cursor-pointer flex items-center justify-center px-4 py-2 rounded-md text-sm font-bold text-white bg-blue-800 hover:bg-blue-600">
                         {{ trans('cookie-consent::texts.agree') }}
                     </button>
-                    <button class="js-cookie-consent-decline cookie-consent__decline cursor-pointer flex items-center justify-center px-4 py-2 rounded-md text-sm font-medium text-white bg-gray-900 hover:bg-gray-600">
+                    <button class="js-cookie-consent-decline cookie-consent__decline cursor-pointer flex items-center justify-center px-4 py-2 rounded-md text-sm font-medium text-white bg-gray-600 hover:bg-gray-500">
                         Decline
                     </button>
                 </div>
